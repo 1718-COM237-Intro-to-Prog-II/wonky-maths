60119f96d7e7b85be61b2efefc41c0e0ec637cc1
Mark Patterson <markpatterson@serc.ac.uk>
2018-05-01 10:59:18 +0100

    added constructor and member function for adding

--- Wonky Maths/WonkyMaths.cs
+++ Wonky Maths/WonkyMaths.cs
@@ -8,5 +8,50 @@ namespace Wonky_Maths
 {
     class WonkyMaths
     {
+        //attributes
+
+        //member functions
+        //add function int
+        public int Add (int a)
+        {
+            return a;
+        }
+        public int Add (int a, int b)
+        {
+            return Convert.ToInt32(this.Add(Convert.ToSingle(a), Convert.ToSingle(b)));
+        }
+        public int Add(int a, int b, int c)
+        {
+            return Convert.ToInt32(this.Add(Convert.ToSingle(a), Convert.ToSingle(b), Convert.ToSingle(c)));
+        }
+        //add function float
+        public float Add (float a)
+        {
+            return a;
+        }
+        public float Add (float a, float b)
+        {
+            return a + b;
+        }
+        public float Add (float a, float b, float c)
+        {
+            return a + b + c;
+        }
+        //add function double
+        public double Add(double a)
+        {
+            return a;
+        }
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public double Add(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+
+        //constructors
+        public WonkyMaths() { }
     }
 }

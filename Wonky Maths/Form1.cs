88db07707b6537b49b033ca7881403d6cbeecf65
Mark Patterson <markpatterson@serc.ac.uk>
2018-05-01 11:25:27 +0100

    added WonkyMaths object to Form1 with some comment description

--- Wonky Maths/Form1.cs
+++ Wonky Maths/Form1.cs
@@ -12,6 +12,18 @@ namespace Wonky_Maths
 {
     public partial class Form1 : Form
     {
+        // Declaring an object M1 of class WonkyMaths
+        WonkyMaths M1 = new WonkyMaths();
+
+        // The WonkyMaths object has the following member functions that need to be tested
+        // M1.Add(int a, int b);
+        // M1.Multiply(float a, float b);
+        // M1.TwoToPowerOf(int a);
+        //
+        // Add and Multiply can take one, two, or three variables.
+        //
+        // Each member function returns the corresponding calculation.
+
         public Form1()
         {
             InitializeComponent();
